7b36a72efa07b05456b4042f1cae24cf9d8c60a1
quantumwebx <131485434+quantumwebx@users.noreply.github.com>
2024-04-26 00:15:44 +0530

    Initial commit

---
+++ src/components/HomeProjects.tsx
@@ -0,0 +1,86 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+
+export const products = [
+  {
+    title: "TRIPWHIZ",
+    link: "https://tripwhizx.vercel.app/",
+    thumbnail: "/courses/tripwhiz.png",
+  },
+  {
+    title: "Quiz-appX",
+    link: "https://quiz-appx.vercel.app/",
+    thumbnail: "/courses/quizapp.png",
+  },
+  {
+    title: "TwitterX-clone",
+    link: "https://twitter-clonex.vercel.app/",
+    thumbnail: "/courses/twitter.png",
+  },
+
+  {
+    title: "2048-Game",
+    link: "https://2048x.vercel.app/",
+    thumbnail: "/courses/2048.png",
+  },
+  {
+    title: "LinkedInX-clone",
+    link: "https://linkedinx.vercel.app/",
+    thumbnail: "/courses/linkedin.png",
+  },
+  {
+    title: "TIC-TAC-TOE-XGAME",
+    link: "https://tic-tac-toe-xgame.vercel.app/",
+    thumbnail: "/courses/tictactoe.png",
+  },
+  {
+    title: "Personal Portfolio",
+    link: "https://Adityakammatiportfolio.vercel.app/",
+    thumbnail: "/courses/personalportfolio.png",
+    },  
+  
+  {
+    title: "TRIPWHIZ",
+    link: "https://tripwhizx.vercel.app/",
+    thumbnail: "/courses/tripwhiz.png",
+  },
+  {
+    title: "Quiz-appX",
+    link: "https://quiz-appx.vercel.app/",
+    thumbnail: "/courses/quizapp.png",
+  },
+  {
+    title: "TwitterX-clone",
+    link: "https://twitter-clonex.vercel.app/",
+    thumbnail: "/courses/twitter.png",
+  },
+
+  {
+    title: "2048-Game",
+    link: "https://2048x.vercel.app/",
+    thumbnail: "/courses/2048.png",
+  },
+  {
+    title: "LinkedInX-clone",
+    link: "https://linkedinx.vercel.app/",
+    thumbnail: "/courses/linkedin.png",
+  },
+  {
+    title: "TIC-TAC-TOE-XGAME",
+    link: "https://tic-tac-toe-xgame.vercel.app/",
+    thumbnail: "/courses/tictactoe.png",
+  },
+  {
+  title: "Personal Portfolio",
+  link: "https://Adityakammatiportfolio.vercel.app/",
+  thumbnail: "/courses/personalportfolio.png",
+  },  
+
+];
+
+function HomeProjects() {
+  return <HeroParallax products={products} />;
+}
+
+export default HomeProjects;
